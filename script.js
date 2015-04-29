c77916f3ddd1bfb201c8b2a8c1876e94cd80fae0
walterlatimer <walter@walterlatimer.com>
2015-04-29 11:59:11 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+var template = function(text) {
+  return '<p><input type="checkbox"><i class="glyphicon glyphicon-star"></i><span>' + text + '</span><i class="glyphicon glyphicon-remove"></i></p>';
+};
+
+var main = function() {
+  $('form').submit(function() {
+      
+    
+    
+    return false;  
+  }
+  
+  
+};
+
+$(document).ready(main);
